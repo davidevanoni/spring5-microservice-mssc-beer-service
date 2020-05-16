3911437123bb36c732c1904abd065bbb4408b170
Davide Vanoni <davide.vanoni@gmail.com>
2020-05-16 12:50:30 +0200

    Added Validation and Error Handling to Controller. Closes #9

--- src/main/java/com/davidevanoni/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/davidevanoni/msscbeerservice/web/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.davidevanoni.msscbeerservice.web.controller;
 import com.davidevanoni.msscbeerservice.web.model.BeerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -19,14 +20,14 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto){
 
         //TODO impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping({"/{beerId}"})
-    public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeer(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto){
 
         //TODO impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
